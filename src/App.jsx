bb08e6a72441110b2b6e3b5659942fbfec9f03c0
Samuelfjr <devsamuelfjr@gmail.com>
2024-08-05 13:10:24 -0300

    Estrutura da aplicação - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import './style.css'
 
 export function App() {
 
   return (
     <>
+      <Header/>
       <Post 
         author="Samuel Fernandes Junior" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sapiente perferendis, id eligendi vitae doloremque, ipsam aperiam iste voluptate, in temporibus quidem reiciendis incidunt! Quisquam consequuntur necessitatibus nostrum vero assumenda!"
