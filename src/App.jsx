2b032b6589a1c4846c3255c8876a4dd25b1a63a2
Samuelfjr <devsamuelfjr@gmail.com>
2024-08-05 09:24:39 -0300

    Iniciando com React - Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ export function App() {
 
   return (
     <>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post 
+        author="Samuel Fernandes Junior" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sapiente perferendis, id eligendi vitae doloremque, ipsam aperiam iste voluptate, in temporibus quidem reiciendis incidunt! Quisquam consequuntur necessitatibus nostrum vero assumenda!"
+      />
+      <Post 
+        author="Eslen Delanogare"
+        content="Post sobre Neurociência!!"
+      />
     </>
   )
 }
