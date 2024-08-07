fa30fa51d672e00477f0b0fde55508fce1b11210
Samuelfjr <devsamuelfjr@gmail.com>
2024-08-07 13:51:47 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
   return (
     <>
       <Header/>
-      <Post 
-        author="Samuel Fernandes Junior" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sapiente perferendis, id eligendi vitae doloremque, ipsam aperiam iste voluptate, in temporibus quidem reiciendis incidunt! Quisquam consequuntur necessitatibus nostrum vero assumenda!"
-      />
-      <Post 
-        author="Eslen Delanogare"
-        content="Post sobre Neurociência!!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Samuel Fernandes Junior" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit sapiente perferendis, id eligendi vitae doloremque, ipsam aperiam iste voluptate, in temporibus quidem reiciendis incidunt! Quisquam consequuntur necessitatibus nostrum vero assumenda!"
+          />
+          <Post 
+            author="Eslen Delanogare"
+            content="Post sobre Neurociência!!"
+          />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover}
+             src="https://images.unsplash.com/photo-1521185496955-15097b20c5fe?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+
+             <div className={styles.profile}>
+                <strong>Samuelfjr</strong>
+                <span>Web Developer</span>
+             </div>
+
+             <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+             </footer>
+        </aside>
+    )
+}
