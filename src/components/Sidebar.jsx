55546b37fecc38066c3ae7aede7af95d96ac27e7
Samuelfjr <devsamuelfjr@gmail.com>
2024-08-09 12:46:29 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -7,12 +9,15 @@ export function Sidebar() {
              src="https://images.unsplash.com/photo-1521185496955-15097b20c5fe?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
 
              <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Samuelfjr.png" alt="" />
+
                 <strong>Samuelfjr</strong>
                 <span>Web Developer</span>
              </div>
 
              <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
              </footer>
