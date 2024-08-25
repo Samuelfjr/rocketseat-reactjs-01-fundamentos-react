6fbc652597740e522289826305d6b3ac4c48c2cc
Samuelfjr <devsamuelfjr@gmail.com>
2024-08-25 16:50:57 -0300

    Os motores do React - Propriedades do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,50 +1,67 @@
-import { Avatar } from './Avatar'
-import { Comment } from './Comment'
-import styles from './Post.module.css'
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-export function Post(props) {
-    return (
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <Avatar src='https://github.com/Samuelfjr.png' />
-                    <div className={styles.authorInfo}>
-                        <strong>Samuelfjr</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
-                
-                <time title='11 de maio às 08:13h' dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
-            </header>
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
+import styles from "./Post.module.css";
 
-            <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a>
-                </p>
-            </div>
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    { locale: ptBR }
+  );
 
-            <form className={styles.commentForm}>
-                <strong>Deixe seu feedback</strong>
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
 
-                <textarea
-                    placeholder='Deixe um comentário'
-                />
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
 
-                <footer>
-                    <button type='submit'>Comentar</button>
-                </footer>
-            </form>
+        <time title={publishedDateFormatted} dateTime="2022-05-11 08:13:30">
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-            <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
-            </div>
-        </article>
-    )
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
 }
